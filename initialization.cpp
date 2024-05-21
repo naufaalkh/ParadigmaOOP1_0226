cab5234a5a78b0ef41c8664bef908e868182cf18
naufaalkh <naufaalkh@gmail.com>
2024-05-21 14:59:37 +0700

    membuat input data

--- initialization.cpp
+++ initialization.cpp
@@ -12,11 +12,17 @@ public:
     mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai){
         // Member Definisi
     }
-    
+
 ~mahasiswa()
     {
         cout << "Id  =" << id << endl;
         cout << "Nama  =" << nama << endl;
         cout << "Nilai  =" << nilai << endl;
     }
+};
+
+int main()
+{
+    mahasiswa mhs(12, "Asroni", 90.5);
+    return 0;
 };
