e8eee0d03bf63ea1ca10c13252eee05b001e2a32
naufaalkh <naufaalkh@gmail.com>
2024-05-21 14:59:13 +0700

    membuat tampilan output mahasiswa

--- initialization.cpp
+++ initialization.cpp
@@ -12,5 +12,11 @@ public:
     mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai){
         // Member Definisi
     }
-
+    
+~mahasiswa()
+    {
+        cout << "Id  =" << id << endl;
+        cout << "Nama  =" << nama << endl;
+        cout << "Nilai  =" << nilai << endl;
+    }
 };
