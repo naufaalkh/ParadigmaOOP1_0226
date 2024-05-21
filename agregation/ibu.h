7ff9d851f7f830ddac1e74bfe9479b575abce64e
naufaalkh <naufaalkh@gmail.com>
2024-05-21 15:56:22 +0700

    membuat konstruktor ibu

--- agregation/ibu.h
+++ agregation/ibu.h
@@ -1,20 +1,19 @@
 #ifndef IBU_H
 #define IBU_H
 
-class ibu {
+class ibu
+{
 public:
     string nama;
-    vector<anak*> daftar_anak;
+    vector<anak *> daftar_anak;
 
-    ibu(string pNama) :nama(pNama) {
+    ibu(string pNama) : nama(pNama)
+    {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
-    ~ibu() {
-        cout << "Ibu \"" << nama << "\" tidak ada\n";
-    }
-    void tambahAnak(anak*);
-    void cetakAnak();
+ 
 };
-void ibu::tambahAnak(anak* pAnak) {
+void ibu::tambahAnak(anak *pAnak)
+{
     daftar_anak.push_back(pAnak);
 }
